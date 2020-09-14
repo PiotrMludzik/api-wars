3445edb218e3cd202b1306af05091ab7fdf785c7
Piotr Młudzik <piotr.mludzik@gmail.com>
2020-09-15 00:31:56 +0200

    chore: set constants

---
+++ constants.py
@@ -0,0 +1,23 @@
+# -------------------------------------------------------------------------------------------------
+#                                             API Wars
+#                                            constants
+#                                              v 1.0
+# -------------------------------------------------------------------------------------------------
+
+# key names
+KEY_PLANETS_CLIMATE = 'climate'
+KEY_PLANETS_DIAMETER = "diameter"
+KEY_PLANETS_NAME = "name"
+KEY_PLANETS_POPULATION = 'population'
+KEY_PLANETS_TERRAIN = 'terrain'
+KEY_PLANETS_WATER = 'surface_water'
+
+# sort order of table headers
+TABLE_COLUMN_ORDER = [
+    KEY_PLANETS_NAME,
+    KEY_PLANETS_DIAMETER,
+    KEY_PLANETS_CLIMATE,
+    KEY_PLANETS_TERRAIN,
+    KEY_PLANETS_WATER,
+    KEY_PLANETS_POPULATION
+]
